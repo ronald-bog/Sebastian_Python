9e6cf3612d8c38123093142b0f3663e037c31689
Ronald Espitia <ronald79@gmail.com>
2025-08-22 16:16:58 -0500

    Clase agosto 22

---
+++ 01-truthy.py
@@ -0,0 +1,15 @@
+
+
+print(bool(0))
+print(bool(""))
+print(bool(False))
+print(bool([]))
+print(bool({}))
+print(bool(None))
+print(bool(()))
+print(bool(set()))
+
+if "jkghkjsdgksdf":
+    print('el IF es True')
+
+
---
+++ 02-metods_str.py
@@ -0,0 +1,23 @@
+# Metodos de los Strings (str)
+
+palabra = "Hola Mundo"
+
+print(palabra[5])
+
+# lower()
+print(palabra.lower())
+
+# upper()
+print(palabra.upper())
+
+# capitalize
+nombre = 'sebastian'
+print(nombre.capitalize())
+
+# split
+planeta = 'Saturno-Venus-Urano'
+print(planeta.split())
+
+# replace
+saludo = 'hola Sebastian'
+print(saludo.replace("Sebastian", "Andres"))
