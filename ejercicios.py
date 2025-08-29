c58e4801006bed02dd65a183bf4500e74aaa5f04
Ronald Espitia <ronald79@gmail.com>
2025-08-29 16:53:15 -0500

    Ejericios

---
+++ ejercicios.py
@@ -0,0 +1,10 @@
+'''
+#! Agosto 29 2025
+
+1. Escribe un programa que lea 5 números enteros e imprima cuántos de ellos son positivos.
+
+2.  Escribe un programa que sume todos los números pares del 1 al 10 e imprima el resultado.
+
+3. Escribe un programa que elija un número secreto entre 1 y 5. El usuario debe intentar adivinarlo. Si acierta, mostrar "¡Correcto!"; si no, "Sigue intentando".
+
+'''
