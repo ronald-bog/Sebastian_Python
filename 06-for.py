d69f6bd72a8f872144acb8f7218e96ff2c1d69ce
Ronald Espitia <ronald79@gmail.com>
2025-09-05 21:55:31 -0500

    Clase sept 9, adjunto ejercicios

--- 06-for.py
+++ 06-for.py
@@ -10,7 +10,7 @@ for variable in secuencia(coleccion):
 planeta = "Saturno y Jupiter"
 lenguajesProgramacion = ['Python', 'Javascript', 'Java', 'C++']
 
-for i in lenguajesProgramacion:
+""" for i in planeta:
     print(i)
 
 # range()
@@ -18,11 +18,11 @@ for i in lenguajesProgramacion:
 for numero in range(6):
     print(numero)
 
-for num in range(2, 9):
+for num in range(39, 54):
     print(num)
 
-for fulanito in range(3, 15, 4):
-    print(fulanito)
+for fulanito in range(31, 55, 5): # 31 36 41 46 51
+    print(fulanito) """
 
 for letra in range(0, 18, 2):
     print(planeta[letra])
@@ -32,7 +32,7 @@ for letra in range(0, 18, 2):
 # Break (break)
 
 for x in range(5):
-    if x == 5:
+    if x == 3:
         break
     print(x)
 
@@ -43,3 +43,41 @@ for z in range(9):
     if z == 5:
         continue
     print(z)
+
+
+# ENUMERATE: obtiene index y elem de una coleccion
+
+saludo = "Hello"
+for index, element in enumerate(saludo):
+    print(f'El indice es: {index} y el elemento es: {element}')
+
+saludo2 = "Hello"
+for idx, e in enumerate(saludo2):
+    print(f'El indice es: {idx} y el elemento es: {e}')
+
+saludo3 = "Hello"  # Ejemplo cambiando el orden logico del indice
+for idx, e in enumerate(saludo3):
+    print(f'El item: {idx + 1} es el elemento: {e}')
+
+# ZIP: Nos permite obtener el elemento de dos colecciones
+
+ciudad = "Medellin"
+estrella = "Sirio"
+
+for e1, e2 in zip(estrella, ciudad):
+    print(e1, e2)
+
+# REVERSE(reversed): Itera en orden inverso
+
+galaxia = "Andromeda"
+
+for l in reversed(galaxia):
+    print(l)
+
+
+# SORTED: Devuelve el iterable ordenado
+
+desordenado = "ypand"
+
+for letra in sorted(desordenado):
+    print(letra)
--- ejercicios.py
+++ ejercicios.py
@@ -7,4 +7,9 @@
 
 3. Escribe un programa que elija un número secreto entre 1 y 5. El usuario debe intentar adivinarlo. Si acierta, mostrar "¡Correcto!"; si no, "Sigue intentando".
 
+
+#! Septiembre 5 2025
+1. Solicita al usuario que escriba una palabra o frase, el programa debe mostrar cada letra de la palabra enumerada en un listado por consola.
+
+2. Solicita al usuario una palabra, despues solicita al usuario una letra a buscar dentro de la palabra, el programa imprimira si la letra existe o no segun sea el caso.
 '''
