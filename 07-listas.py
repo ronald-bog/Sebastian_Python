f38fcdb02f0ef9707d164766f3d7c827f3e0cee1
Ronald Espitia <ronald79@gmail.com>
2025-09-09 17:36:09 -0500

    Clase Sept 8

---
+++ 07-listas.py
@@ -0,0 +1,19 @@
+# lista
+
+# creacion de una lista
+
+numeros = [20, 5, 8, 9, 48]
+
+# print(numeros[5])  # esto genera error
+print(numeros[2])  # esto imprime el 8
+
+# Metodos de las listas
+
+# longitud de una lista, len()
+print(len(numeros))
+
+# append(): Agregar elementos al final de una lista
+
+numeros.append(100)
+
+print(numeros)
