fda573ae75604fe8c56469ba39110226f20e7455
Ronald Espitia <ronald79@gmail.com>
2025-09-13 11:21:19 -0500

    Clase sept 12 y ejercicios

--- 07-listas.py
+++ 07-listas.py
@@ -6,6 +6,9 @@ numeros = [20, 5, 8, 9, 48]
 
 # print(numeros[5])  # esto genera error
 print(numeros[2])  # esto imprime el 8
+numeros[3] = 100  # Cambiamos el elemento de la lista segun su indice
+
+print(numeros)
 
 # Metodos de las listas
 
@@ -17,3 +20,66 @@ print(len(numeros))
 numeros.append(100)
 
 print(numeros)
+
+# extend:
+listaA = [8, 10]
+listaB = [56, 65]
+listaA.extend(listaB)
+print(listaA)
+
+# insert :
+list1 = [60, 20, 55]
+list1.insert(2, 2000)
+print(list1)
+
+# remove
+miLista = ['sol', 'luna', 'marte', 'luna', 'jupiter']
+miLista.remove('luna')
+print(miLista)
+
+# pop: elimina el ultimo elemento
+misPlanetas = ['tierra', 'saturno', 'marte', 'urano', 'jupiter']
+# misPlanetas.pop()
+devuelto = misPlanetas.pop(1)
+
+print(misPlanetas)
+print('---------------------')
+print(devuelto)
+
+# index: Devuelve el indice del primer elemento cuyo valor es igual al elemento pasado como argumento
+
+numbers = [10, 20, 30, 40, 30]
+# print(numbers.index(30))
+# print(numbers)
+
+# count:
+veces = numbers.count(30)
+print(veces)
+
+nombres = ['Lina', 'Sebas', 'Roger', 'Andres', 'Andres', 'Ronald', 'Andres']
+
+print(nombres.count('Andres'))
+
+# sort: por defecto ordena en ascendente
+nums = [54, 6, 8, 778, 10]
+# nums.sort()
+nums.sort(reverse=True)  # descendente
+print(nums)
+
+print(nums[0])
+
+# copy
+
+valores = [54, 6, 8, 778, 10]
+
+newValores = valores.copy()
+
+print(valores)
+print(newValores)
+
+# clear
+estudiantes = ['Lina', 'Sebas', 'Roger',
+               'Andres', 'Andres', 'Ronald', 'Andres']
+print(estudiantes)
+estudiantes.clear()
+print(estudiantes)
--- ejercicios.py
+++ ejercicios.py
@@ -12,4 +12,19 @@
 1. Solicita al usuario que escriba una palabra o frase, el programa debe mostrar cada letra de la palabra enumerada en un listado por consola.
 
 2. Solicita al usuario una palabra, despues solicita al usuario una letra a buscar dentro de la palabra, el programa imprimira si la letra existe o no segun sea el caso.
+
+#! Septiembre 12 2025
+1. Crea un codigo que recorra la lista 'numbers' y agregue a la lista 'pares' solo los numeros pares. Tip: una lista se recorre con for igual que como lo harias con un string.
+
+    numbers = [23, 425, 1567, 89, 372, 914, 6432, 128, 773, 504, 339, 2845, 951, 62, 4178, 723, 194, 846, 5521, 367, 679, 2243, 792, 48, 5316, 689, 317, 9024, 175, 468]
+    
+    pares = []
+    
+    Finalmente imprimir como quedo la lista 'pares'.
+
+
+2. Crea un codigo que sume todos los numeros dentro de la lista 'valores' y mostrar el resultado.
+
+valores = [523, 78,  43, 651, 1023, 44, 200, 8, 74, 31, 8521, 746, 400, 572]
+
 '''
